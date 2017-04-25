73dd34a87fc756e3c43391491f5b2cccfd1843b8
Gab13 <gabriela.r1303@gmail.com>
2017-04-25 15:46:34 +0100

    Add files via upload
    Updated midi keyboard handlers

---
+++ MidiHandlerInitialiser.java
@@ -0,0 +1,12 @@
+package VirtualPiano;
+import java.io.FileNotFoundException;
+
+public class MidiHandlerInitialiser {
+	
+
+public static void main(String[] args) throws FileNotFoundException {
+	MidiHandler midiIn = new MidiHandler();
+	
+}
+
+}

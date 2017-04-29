437910951faeacb0097d7b3939895cbee21716ff
Gab13 <gabriela.r1303@gmail.com>
2017-04-29 20:12:55 +0100

    Add files via upload
    Updated Track Player

--- TrackPlayer.java
+++ TrackPlayer.java
@@ -1,17 +1,25 @@
 package VirtualPiano;
 
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+
 //This class reads in note info from a txt file and plays the notes
+
 public class TrackPlayer {
 	String noteInfo;
 	int size = 9;
@@ -21,38 +29,25 @@ public class TrackPlayer {
 	String noteOctave;
 	String noteDuration;
 	double dur;
+	private double speed;
+	private String sequence;
+	private int volume = 1;
+
 	
-	public void readTrack() throws IOException, InterruptedException {
-		try (BufferedReader br = new BufferedReader(new FileReader("trackOutput.txt"))) {
-		    String line;
-		    while ((line = br.readLine()) != null) {
-		    	note = new char[6];
-		    	int j = 0;
+	public void readTrack() throws InterruptedException {
+
+		    String [] seq = sequence.split(";");
+
+		    for (int x = 0; x < seq.length; x++) {
 		    	
-		    	noteInfo = line.toString();
+		    	String [] seq2 = seq[x].split(",");
+
+		    	dur = Double.parseDouble(seq2[3]);
 		    	
-		    	for (int i = 0;i < size; i++){
-		    		char temp = noteInfo.charAt(i);
-		    		
-		    		if(temp != ';') {
-		    	    note[j] = temp;	
-		    	    j++;
-		    		}	
-		    	}
-		    	
-		    	noteNumber = "" + note[0];
-		    	noteType = "" + note[1];
-		    	noteOctave = "" + note[2];
-		    	
-		    	noteDuration = "" + note[3] + note[4] + note[5];
-		    	
-		    	dur = Double.parseDouble(noteDuration);
-		    	
-		    	String note = transformToNoteName(noteNumber, noteType, noteOctave);
+		    	String note = transformToNoteName(seq2[0], seq2[1], seq2[2]);
 		    	playAudio(note, dur);
 		    }
 		}
-	}
 	public String transformToNoteName(String number, String type, String oct) {
 		String noteInfo = "";
 		
@@ -86,21 +81,25 @@ public class TrackPlayer {
 
 	public void playAudio(String info, double d) throws InterruptedException {
 		String audio = null;
-		boolean stop = false;
 		
 		audio = info + ".wav"; 
 		
 		AudioInputStream audioInputStream;
 		try {
 			audioInputStream = AudioSystem.getAudioInputStream(new File(audio).getAbsoluteFile());
-		Clip clip = AudioSystem.getClip();
-		clip.open(audioInputStream);
-		clip.start();
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+			gainControl.setValue(volume);
+			clip.start();
 		
 			long tStart = 0;
 			long tEnd = 0;
 			double duration;
 			long tDelta;
+			
+			double division = d/speed;
+			division = Math.round(division * 100.0) / 100.0;
 			
 			tStart = System.currentTimeMillis();
 			do{
@@ -109,7 +108,7 @@ public class TrackPlayer {
         	tDelta = tEnd - tStart;
         	duration = tDelta / 1000.0;
 
-			} while (duration != d);
+			} while (duration != division);
 
 		
 		} catch (UnsupportedAudioFileException | IOException e1) {
@@ -119,8 +118,31 @@ public class TrackPlayer {
 		}
 	}
 	
-	public static void main(String[] args) throws IOException, InterruptedException {
-		TrackPlayer player = new TrackPlayer();
-		player.readTrack();
+	public void setVolume(int pasVol){
+		volume = pasVol;
 	}
-}
+	
+	public int getVolume(){
+		return volume;
+	}
+	public void setSpeed(double pasSpeed){
+		speed = pasSpeed;
+	}
+	
+	public double getSpeed(){
+		return speed;
+	}
+	
+	public void printSpeed () {
+		System.out.println(speed);
+	}
+	
+	public void setSequence(String pasSequence){
+		sequence = pasSequence;
+	}
+	
+	public String getSequence(){
+		return sequence;
+	}
+	
+}	

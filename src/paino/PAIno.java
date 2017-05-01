86c46276f0fe38fa08f707dc803eccf62f53e902
Alexander Daskalov <rjuan33@gmail.com>
2017-05-01 16:10:44 +0100

    Add files via upload

---
+++ src/paino/PAIno.java
@@ -0,0 +1,18 @@
+package paino;
+
+import java.sql.SQLException;
+import javax.swing.SwingUtilities;
+
+public class PAIno {
+    public static DBController db;
+    public static void main(String[] args) throws SQLException {
+        db = new DBController("storage");
+        //db.initDB();
+        SwingUtilities.invokeLater(() -> {
+            VirtualPiano frame;
+            frame = new VirtualPiano();
+            frame.setVisible(true);
+        });
+    }
+
+}
